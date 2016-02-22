33829f21fbf3affb5cade5b5e8db08613ebe177e
Peter Schwartz <pschwart@gustavus.edu>
2016-02-21 23:25:10 -0600

    testCommit

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,22 +1,36 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
+    private static final String KEY_CHEATER = "cheater";
+
 
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mPreviousButton;
     private ImageButton mNextButton;
     private TextView mQuestionTextView;
+    private Button mCheatButton;
 
-    private Question[] mQuestionBank = new Question[] {
+    private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
             new Question(R.string.question_africa, false),
@@ -25,6 +39,10 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
+    private boolean[] mCheatBank = new boolean[mQuestionBank.length];
+    private String[] mCheatString = new String[mQuestionBank.length+1];
+
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
@@ -33,33 +51,37 @@ public class QuizActivity extends AppCompatActivity {
 
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-
         int messageResId = 0;
-
-        if(userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+        mIsCheater = mCheatBank[mCurrentIndex];
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
         } else {
-            messageResId = R.string.incorrect_toast;
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
 
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
     }
 
     private void nextQuestion() {
-        mCurrentIndex = (mCurrentIndex+1)%mQuestionBank.length;
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
         updateQuestion();
     }
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View V) {
+            public void onClick(View v) {
                 nextQuestion();
             }
         });
@@ -71,7 +93,6 @@ public class QuizActivity extends AppCompatActivity {
                 checkAnswer(true);
             }
         });
-
 
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
@@ -92,16 +113,75 @@ public class QuizActivity extends AppCompatActivity {
 
         mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater = false;
                 updateQuestion();
-        }
-    });
+            }
+        });
+
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent i = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(i, REQUEST_CODE_CHEAT);
+            }
+        });
+
 
         updateQuestion();
-
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (resultCode == REQUEST_CODE_CHEAT){
+            if (data == null) {
+                return;
+            }
 
+        }
+        mCheatBank[mCurrentIndex] = CheatActivity.wasAnswerShown(data);
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        //Second Challenge :Just saved the value of mIsCheater into an array of
+        // boolean values, so that when the user rotates the screen after they get back, mIsCheater is not cleared out.
+        //Third Challenge: I used the mCheatBank array to store the val of mIsCheater, because I wanted to make sure
+        // the user could back to the other questions with cleared cheating result.
+        for (int i = 1; i<=mQuestionBank.length;i++) {
+            mCheatString[i] = "CHEATSTRING"+i;
+            savedInstanceState.putBoolean(mCheatString[i],mCheatBank[i-1]);
+        }
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
 }
